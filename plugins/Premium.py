7b4f77edee2a4055cf0eac8b037af49521ece520
HectraCore <hectracore@gmail.com>
2026-05-30 02:16:10 +0400

    Add files via upload

---
+++ plugins/Premium.py
@@ -0,0 +1,168 @@
+from datetime import timedelta
+import pytz
+import datetime, time
+from Script import script 
+from info import *
+from utils import get_seconds
+from database.users_chats_db import db 
+from pyrogram import Client, filters 
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@Client.on_message(filters.command("remove_premium") & filters.user(ADMINS))
+async def remove_premium(client, message):
+    if len(message.command) == 2:
+        user_id = int(message.command[1])  # Convert the user_id to integer
+        user = await client.get_users(user_id)
+        if await db.remove_premium_access(user_id):
+            await message.reply_text("ᴜꜱᴇʀ ʀᴇᴍᴏᴠᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ !")
+            await client.send_message(
+                chat_id=user_id,
+                text=f"<b>ʜᴇʏ {user.mention},\n\n<blockquote>𝒀𝒐𝒖𝒓 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 𝑨𝒄𝒄𝒆𝒔𝒔 𝑯𝒂𝒔 𝑩𝒆𝒆𝒏 𝑹𝒆𝒎𝒐𝒗𝒆𝒅. 𝑻𝒉𝒂𝒏𝒌 𝒀𝒐𝒖 𝑭𝒐𝒓 𝑼𝒔𝒊𝒏𝒈 𝑶𝒖𝒓 𝑺𝒆𝒓𝒗𝒊𝒄𝒆 😊. 𝑪𝒍𝒊𝒄𝒌 𝑶𝒏 /plan 𝑻𝒐 𝑪𝒉𝒆𝒄𝒌 𝑶𝒖𝒓 𝑶𝒕𝒉𝒆𝒓 𝑷𝒍𝒂𝒏𝒔.</blockquote></b>"
+            )
+        else:
+            await message.reply_text("ᴜɴᴀʙʟᴇ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴜꜱᴇᴅ !\nᴀʀᴇ ʏᴏᴜ ꜱᴜʀᴇ, ɪᴛ ᴡᴀꜱ ᴀ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀ ɪᴅ ?")
+    else:
+        await message.reply_text("ᴜꜱᴀɢᴇ : /remove_premium user_id") 
+
+@Client.on_message(filters.command("myplan"))
+async def myplan(client, message):
+    user = message.from_user.mention 
+    user_id = message.from_user.id
+    data = await db.get_user(message.from_user.id)  # Convert the user_id to integer
+    if data and data.get("expiry_time"):
+        #expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=data)
+        expiry = data.get("expiry_time") 
+        expiry_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata"))
+        expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
+        # Calculate time difference
+        current_time = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        time_left = expiry_ist - current_time
+            
+        # Calculate days, hours, and minutes
+        days = time_left.days
+        hours, remainder = divmod(time_left.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+            
+        # Format time left as a string
+        time_left_str = f"{days} Dᴀʏꜱ, {hours} Hᴏᴜʀꜱ, {minutes} Mɪɴᴜᴛᴇꜱ"
+        await message.reply_text(f"⚜️ Pʀᴇᴍɪᴜᴍ Uꜱᴇʀ Dᴀᴛᴀ :\n\n👤 Uꜱᴇʀ : {user}\n⚡ Uꜱᴇʀ Iᴅ : <code>{user_id}</code>\n⏰ Tɪᴍᴇ Lᴇꜰᴛ : {time_left_str}\n⌛️ Exᴘɪʀʏ Dᴀᴛᴇ : {expiry_str_in_ist}")   
+    else:
+        await message.reply_text(f"<b>Hᴇʏ {user},\n\n<blockquote>𝒀𝒐𝒖 𝑫𝒐 𝑵𝒐𝒕 𝑯𝒂𝒗𝒆 𝑨𝒏𝒚 𝑨𝒄𝒕𝒊𝒗𝒆 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 𝒑𝒍𝒂𝒏𝒔, 𝑰𝒇 𝒀𝒐𝒖 𝑾𝒂𝒏𝒕 𝑻𝒐 𝑻𝒂𝒌𝒆 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 𝑻𝒉𝒆𝒏 𝑪𝒍𝒊𝒄𝒌 𝑶𝒏 𝑩𝒆𝒍𝒐𝒘 𝑩𝒖𝒕𝒕𝒐𝒏 👇</blockquote><b>",
+	reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🌳 Cʜᴇᴄᴋᴏᴜᴛ Pʀᴇᴍɪᴜᴍ Pʟᴀɴꜱ 🌳", callback_data='seeplans')]]))			 
+
+@Client.on_message(filters.command("get_premium") & filters.user(ADMINS))
+async def get_premium(client, message):
+    if len(message.command) == 2:
+        user_id = int(message.command[1])
+        user = await client.get_users(user_id)
+        data = await db.get_user(user_id)
+
+        if data and data.get("expiry_time"):
+            expiry = data.get("expiry_time")
+            expiry_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata"))
+            expiry_str_in_ist = expiry_ist.strftime(
+                "%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p"
+            )
+
+            current_time = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+            time_left = expiry_ist - current_time
+
+            days = time_left.days
+            hours, remainder = divmod(time_left.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+
+            time_left_str = f"{days} Dᴀʏꜱ, {hours} Hᴏᴜʀꜱ, {minutes} Mɪɴᴜᴛᴇꜱ"
+
+            await message.reply_text(
+                f"⚜️ Pʀᴇᴍɪᴜᴍ Uꜱᴇʀ Dᴀᴛᴀ :\n\n"
+                f"👤 Uꜱᴇʀ : {user.mention}\n"
+                f"⚡ Uꜱᴇʀ Iᴅ : <code>{user_id}</code>\n"
+                f"⏰ Tɪᴍᴇ Lᴇꜰᴛ : {time_left_str}\n"
+                f"⌛️ Exᴘɪʀʏ Dᴀᴛᴇ : {expiry_str_in_ist}"
+            )
+        else:
+            await message.reply_text(
+                "Nᴏ Aɴʏ Pʀᴇᴍɪᴜᴍ Dᴀᴛᴀ Fᴏᴜɴᴅ Iɴ Dᴀᴛᴀʙᴀꜱᴇ !"
+            )
+    else:
+        await message.reply_text("ᴜꜱᴀɢᴇ : /get_premium user_id")
+
+@Client.on_message(filters.command("add_premium") & filters.user(ADMINS))
+async def give_premium_cmd_handler(client, message):
+    if len(message.command) == 4:
+        time_zone = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        current_time = time_zone.strftime("%d-%m-%Y\n⏱️ ᴊᴏɪɴɪɴɢ ᴛɪᴍᴇ : %I:%M:%S %p") 
+        user_id = int(message.command[1])  
+        user = await client.get_users(user_id)
+        time = message.command[2]+" "+message.command[3]
+        seconds = await get_seconds(time)
+        if seconds > 0:
+            expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+            user_data = {"id": user_id, "expiry_time": expiry_time}  
+            await db.update_user(user_data) 
+            data = await db.get_user(user_id)
+            expiry = data.get("expiry_time")   
+            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ Exᴘɪʀʏ Tɪᴍᴇ : %I:%M:%S %p")         
+            await message.reply_text(f"Pʀᴇᴍɪᴜᴍ Aᴅᴅᴇᴅ Sᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ✅\n\n👤 Uꜱᴇʀ : {user.mention}\n⚡ Uꜱᴇʀ Iᴅ : <code>{user_id}</code>\n⏰ Pʀᴇᴍɪᴜᴍ Aᴄᴄᴇꜱꜱ : <code>{time}</code>\n\n⏳ Jᴏɪɴɪɴɢ Dᴀᴛᴇ : {current_time}\n\n⌛️ Exᴘɪʀʏ Dᴀᴛᴇ : {expiry_str_in_ist}", disable_web_page_preview=True)
+            await client.send_message(
+                chat_id=user_id,
+                text=f"👋 Hᴇʏ {user.mention},\nTʜᴀɴᴋ Yᴏᴜ Fᴏʀ Pᴜʀᴄʜᴀꜱɪɴɢ Pʀᴇᴍɪᴜᴍ.\nEɴᴊᴏʏ !! ✨🎉\n\n⏰ Pʀᴇᴍɪᴜᴍ Aᴄᴄᴇꜱꜱ : <code>{time}</code>\n⏳ Jᴏɪɴɪɴɢ Dᴀᴛᴇ : {current_time}\n\n⌛️ Exᴘɪʀʏ Dᴀᴛᴇ : {expiry_str_in_ist}", disable_web_page_preview=True              
+            )    
+            await client.send_message(PREMIUM_LOGS, text=f"#Added_Premium\n\n👤 Uꜱᴇʀ : {user.mention}\n⚡ Uꜱᴇʀ Iᴅ : <code>{user_id}</code>\n⏰ Pʀᴇᴍɪᴜᴍ Aᴄᴄᴇꜱꜱ : <code>{time}</code>\n\n⏳ Jᴏɪɴɪɴɢ Dᴀᴛᴇ : {current_time}\n\n⌛️ Exᴘɪʀʏ Dᴀᴛᴇ : {expiry_str_in_ist}", disable_web_page_preview=True)
+                    
+        else:
+            await message.reply_text("Invalid time format. Please use '1 day for days', '1 hour for hours', or '1 min for minutes', or '1 month for months' or '1 year for year'")
+    else:
+        await message.reply_text("Usage : /add_premium user_id time (e.g., '1 day for days', '1 hour for hours', or '1 min for minutes', or '1 month for months' or '1 year for year')")
+
+@Client.on_message(filters.command("premium_users") & filters.user(ADMINS))
+async def premium_user(client, message):
+    aa = await message.reply_text("<i>ꜰᴇᴛᴄʜɪɴɢ...</i>")
+    new = f" ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀꜱ ʟɪꜱᴛ :\n\n"
+    user_count = 1
+    users = await db.get_all_users()
+    async for user in users:
+        data = await db.get_user(user['id'])
+        if data and data.get("expiry_time"):
+            expiry = data.get("expiry_time") 
+            expiry_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata"))
+            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
+            current_time = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+            time_left = expiry_ist - current_time
+            days = time_left.days
+            hours, remainder = divmod(time_left.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            time_left_str = f"{days} days, {hours} hours, {minutes} minutes"	 
+            new += f"{user_count}. {(await client.get_users(user['id'])).mention}\n👤 ᴜꜱᴇʀ ɪᴅ : {user['id']}\n⏳ ᴇxᴘɪʀʏ ᴅᴀᴛᴇ : {expiry_str_in_ist}\n⏰ ᴛɪᴍᴇ ʟᴇꜰᴛ : {time_left_str}\n"
+            user_count += 1
+        else:
+            pass
+    try:    
+        await aa.edit_text(new)
+    except MessageTooLong:
+        with open('usersplan.txt', 'w+') as outfile:
+            outfile.write(new)
+        await message.reply_document('usersplan.txt', caption="Paid Users:")
+
+
+
+@Client.on_message(filters.command("plan"))
+async def plan(client, message):
+    user_id = message.from_user.id 
+    users = message.from_user.mention 
+    btn = [[
+            InlineKeyboardButton('• Rᴇғᴇʀ •', callback_data='reffff')
+        ],[
+            InlineKeyboardButton('• Gᴏʟᴅ ', callback_data='gold'),
+            InlineKeyboardButton('• Pʟᴀᴛɪɴᴜᴍ ', callback_data='platinum')
+        ],[
+            InlineKeyboardButton('• Dɪᴀᴍᴏɴᴅ ', callback_data='diamond'),
+            InlineKeyboardButton('• Oᴛʜᴇʀ ', callback_data='other')
+        ],[
+            InlineKeyboardButton('• Fʀᴇᴇ Tʀɪᴀʟ ', callback_data='free')
+        ],[            
+            InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
+    ]]
+    await message.reply_photo(photo="https://graph.org/file/7d8b428734782477158d4.jpg", caption=script.PREMIUM_TEXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
+    
---
+++ plugins/Verification.py
@@ -0,0 +1,104 @@
+from pyrogram import *
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from database.verify_db import vr_db 
+from info import ADMINS
+from datetime import datetime
+
+@Client.on_message(filters.command("verification") & filters.private & filters.user(ADMINS))
+async def vrfs(client, message):
+    today = await vr_db.get_vr_count("today")
+    yesterday = await vr_db.get_vr_count("yesterday")
+    this_week = await vr_db.get_vr_count("this_week")
+    this_month = await vr_db.get_vr_count("this_month")
+    last_month = await vr_db.get_vr_count("last_month")
+    this_year = await vr_db.get_vr_count("year", year=datetime.now().year)
+    last_year = await vr_db.get_vr_count("year", year=datetime.now().year - 1)
+
+    btn = [[
+        InlineKeyboardButton("Tᴏᴅᴀʏ", callback_data=f'vrrfrs#tud'), 
+        InlineKeyboardButton(f"{today}", callback_data=f'vrrfrs#tud')
+        ],[
+        InlineKeyboardButton("Yᴇsᴛᴇʀᴅᴀʏ", callback_data=f'vrrfrs#yes'), 
+        InlineKeyboardButton(f"{yesterday}", callback_data=f'vrrfrs#yes')
+        ],[
+        InlineKeyboardButton("Tʜɪs Wᴇᴇᴋ", callback_data=f'vrrfrs#week'), 
+        InlineKeyboardButton(f"{this_week}", callback_data=f'vrrfrs#week')     
+        ],[
+        InlineKeyboardButton("Tʜɪs Mᴏɴᴛʜ", callback_data=f'vrrfrs#mont'), 
+        InlineKeyboardButton(f"{this_month}", callback_data=f'vrrfrs#mont')
+        ],[
+        InlineKeyboardButton("Lᴀsᴛ Mᴏɴᴛʜ", callback_data=f'vrrfrs#lmont'), 
+        InlineKeyboardButton(f"{last_month}", callback_data=f'vrrfrs#lmont')        
+        ],[
+        InlineKeyboardButton("Tʜɪs Yᴇᴀʀ", callback_data=f'vrrfrs#tyear'), 
+        InlineKeyboardButton(f"{this_year}", callback_data=f'vrrfrs#tyear')
+        ],[
+        InlineKeyboardButton("Lᴀsᴛ Yᴇᴀʀ", callback_data=f'vrrfrs#lyear'), 
+        InlineKeyboardButton(f"{last_year}", callback_data=f'vrrfrs#lyear')
+        ],[
+        InlineKeyboardButton("🔄 Rᴇғʀᴇsʜ", callback_data=f'vrrfrs#vrrfrs'), 
+    ]]
+    await message.reply_text("✅ **#verification**\n\nTᴏᴛᴀʟ Vᴇʀɪғɪᴇᴅ Usᴇʀs", reply_markup=InlineKeyboardMarkup(btn))
+
+@Client.on_callback_query(filters.regex(r"^vrrfrs"))
+async def vr_ref(client, query): 
+    ident, set_type = query.data.split("#")
+
+    if set_type == "tud":
+        return await query.answer('Verified Users From Today', show_alert=True)
+    elif set_type == "yes":
+        return await query.answer('Verified Users From Yesterday', show_alert=True)
+    elif set_type == "week":
+        return await query.answer('Verified Users From This Week', show_alert=True)
+    elif set_type == "mont":
+        return await query.answer('Verified Users From This Month', show_alert=True)
+    elif set_type == "lmont":
+        return await query.answer('Verified Users From Last Month', show_alert=True)
+    elif set_type == "tyear":
+        return await query.answer('Verified Users From This Year', show_alert=True)
+    elif set_type == "lyear":
+        return await query.answer('Verified Users From Last Year', show_alert=True)
+    else:#set_type == "vrrfrs":   
+        pass 
+        
+    # Refresh the data
+    today = await vr_db.get_vr_count("today")
+    yesterday = await vr_db.get_vr_count("yesterday")
+    this_week = await vr_db.get_vr_count("this_week")
+    this_month = await vr_db.get_vr_count("this_month")
+    last_month = await vr_db.get_vr_count("last_month")
+    this_year = await vr_db.get_vr_count("year", year=datetime.now().year)
+    last_year = await vr_db.get_vr_count("year", year=datetime.now().year - 1)
+    
+    btn = [[
+        InlineKeyboardButton("Tᴏᴅᴀʏ", callback_data=f'vrrfrs#tud'), 
+        InlineKeyboardButton(f"{today}", callback_data=f'vrrfrs#tud')
+        ],[
+        InlineKeyboardButton("Yᴇsᴛᴇʀᴅᴀʏ", callback_data=f'vrrfrs#yes'), 
+        InlineKeyboardButton(f"{yesterday}", callback_data=f'vrrfrs#yes')
+        ],[
+        InlineKeyboardButton("Tʜɪs Wᴇᴇᴋ", callback_data=f'vrrfrs#week'), 
+        InlineKeyboardButton(f"{this_week}", callback_data=f'vrrfrs#week')     
+        ],[
+        InlineKeyboardButton("Tʜɪs Mᴏɴᴛʜ", callback_data=f'vrrfrs#mont'), 
+        InlineKeyboardButton(f"{this_month}", callback_data=f'vrrfrs#mont')
+        ],[
+        InlineKeyboardButton("Lᴀsᴛ Mᴏɴᴛʜ", callback_data=f'vrrfrs#lmont'), 
+        InlineKeyboardButton(f"{last_month}", callback_data=f'vrrfrs#lmont')        
+        ],[
+        InlineKeyboardButton("Tʜɪs Yᴇᴀʀ", callback_data=f'vrrfrs#tyear'), 
+        InlineKeyboardButton(f"{this_year}", callback_data=f'vrrfrs#tyear')
+        ],[
+        InlineKeyboardButton("Lᴀsᴛ Yᴇᴀʀ", callback_data=f'vrrfrs#lyear'), 
+        InlineKeyboardButton(f"{last_year}", callback_data=f'vrrfrs#lyear')
+        ],[
+        InlineKeyboardButton("🔄 Rᴇғʀᴇsʜ", callback_data=f'vrrfrs#vrrfrs'), 
+    ]] 
+    try: 
+        await query.message.edit("✅ **#verification**\n\nTᴏᴛᴀʟ Vᴇʀɪғɪᴇᴅ Usᴇʀs", reply_markup=InlineKeyboardMarkup(btn))
+    except MessageNotModified:
+        pass
+    await query.answer("Rᴇғʀᴇsʜɪɴɢ_Dᴀᴛᴀ ✅......")
+
+  
