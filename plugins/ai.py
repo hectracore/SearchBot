4eef7a0b680cacdd24bf11100cb8e0e33b8bad71
HectraCore <hectracore@gmail.com>
2026-05-30 02:23:33 +0400

    Add files via upload

---
+++ plugins/ai.py
@@ -0,0 +1,28 @@
+import requests
+from pyrogram import Client, filters
+import time
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+from MukeshAPI import api
+@Client.on_message(filters.command(["chatgpt"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat_gpt(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "Example:**\n\n`/chatgpt How To Set Gf`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @SRSMOVIEBOT ", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+
+__mod_name__ = "Cʜᴀᴛɢᴘᴛ"
+__help__ = """
+ Cʜᴀᴛɢᴘᴛ ᴄᴀɴ ᴀɴsᴡᴇʀ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ  ᴀɴᴅ sʜᴏᴡs ʏᴏᴜ ᴛʜᴇ ʀᴇsᴜʟᴛ
+
+ ❍ /chatgpt  *:* ʀᴇᴘʟʏ ᴛo ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ
+ 
+ """
